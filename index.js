7ecda7cdac2e40c46f7d5e0fd18140e24429b247
Eren Topaçoğlu <93638566+Reactive21@users.noreply.github.com>
2022-08-27 20:59:04 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,37 +1,22 @@
-// let counterDisplayElem = document.querySelector('.counter-display');
-// let counterMinusElem = document.querySelector('.counter-minus');
-// let counterPlusElem = document.querySelector('.counter-plus');
+let counterDisplayElem = document.querySelector('.counter-display');
+let counterMinusElem = document.querySelector('.counter-minus');
+let counterPlusElem = document.querySelector('.counter-plus');
 
-// let count = 0;
+let count = 0;
 
-// updateDisplay();
+updateDisplay();
 
-// counterPlusElem.addEventListener("click",()=>{
-//     count++;
-//     updateDisplay();
-// }) ;
+counterPlusElem.addEventListener("click",()=>{
+    count++;
+    updateDisplay();
+}) ;
 
-// counterMinusElem.addEventListener("click",()=>{
-//     count--;
-//     updateDisplay();
-// });
+counterMinusElem.addEventListener("click",()=>{
+    count--;
+    updateDisplay();
+});
 
-// function updateDisplay(){
-//     counterDisplayElem.innerHTML = count;
-// };
+function updateDisplay(){
+    counterDisplayElem.innerHTML = count;
+};
 
-let counter = 0
-let counterDOM = document.querySelector("#counter")
-let increaseDOM = document.querySelector("#increase")
-let decreaseDOM = document.querySelector("#decrease")
-
-counterDOM.innerHTML=counter
-
-increaseDOM.addEventListener("click", function(){
-    counter++;
-    counterDOM.innerHTML=counter
-})
-decreaseDOM.addEventListener("click", function(){
-    counter--;
-    counterDOM.innerHTML=counter
-})
